bdae113a07eef823441215062a92c8309367e104
deepson <deepsongbtl@gmail.com>
2026-05-25 16:51:14 +0000

    world 7th and 8th

--- Scripts/area_2d.gd
+++ Scripts/area_2d.gd
@@ -5,8 +5,17 @@ func _on_body_entered(body):
 	if body.is_in_group("player"):
 		var current_scene = get_tree().current_scene.scene_file_path
 		if current_scene == "res://Scene/world.tscn":
+			get_tree().change_scene_to_file("res://Scene/world_5.tscn")
+		elif current_scene == "res://Scene/world_5.tscn":
+			get_tree().change_scene_to_file("res://Scene/world_7.tscn")
+		elif current_scene == "res://Scene/world_7.tscn":
+			get_tree().change_scene_to_file("res://Scene/world_6.tscn")
+		elif current_scene == "res://Scene/world_6.tscn":
+			get_tree().change_scene_to_file("res://Scene/world_8.tscn")
+		elif current_scene == "res://Scene/world_8.tscn":
 			get_tree().change_scene_to_file("res://Scene/world_2.tscn")
 		elif current_scene == "res://Scene/world_2.tscn":
 			get_tree().change_scene_to_file("res://Scene/world_3.tscn")
 		elif current_scene == "res://Scene/world_3.tscn":
 			get_tree().change_scene_to_file("res://Scene/world_4.tscn")
+			
