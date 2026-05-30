4cccd723a3d6ba61ba1eedc272df4602ee181e87
deepson <deepsongbtl@gmail.com>
2026-05-30 09:08:59 +0000

    Levels code

---
+++ Scripts/canvas_layer4.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+
+
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scene/main_menu.tscn")
+
+
+func _on_texture_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scene/world_4.tscn")
--- Scripts/selection.gd
+++ Scripts/selection.gd
@@ -4,3 +4,28 @@ extends Control
 func _ready():
 	$CanvasLayer/portal.play("idle")
 	$CanvasLayer/AnimatedSprite2D.play("player")
+
+
+func _on__pressed() -> void:
+	get_tree().change_scene_to_file("res://Scene/world.tscn")
+
+func _on__pressed2() -> void:
+	get_tree().change_scene_to_file("res://Scene/world_5.tscn")
+
+func _on__pressed3() -> void:
+	get_tree().change_scene_to_file("res://Scene/world_7.tscn")
+
+func _on__pressed4() -> void:
+	get_tree().change_scene_to_file("res://Scene/world_6.tscn")
+	
+func _on__pressed5() -> void:
+	get_tree().change_scene_to_file("res://Scene/world_8.tscn")
+
+func _on__pressed6() -> void:
+	get_tree().change_scene_to_file("res://Scene/world_2.tscn")
+
+func _on__pressed7() -> void:
+	get_tree().change_scene_to_file("res://Scene/world_3.tscn")
+
+func _on__pressed8() -> void:
+	get_tree().change_scene_to_file("res://Scene/world_4.tscn")
